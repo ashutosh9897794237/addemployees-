9108a1e256605e3584368a63978126a0b9b85d81
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-01-15 01:05:40 +0530

    from

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let btnAdd = document.querySelector('button');
 let table = document.querySelector('table');
 let nameInput = document.querySelector('#name');
 let ageInput = document.querySelector('#age');
-let countryInput = document.querySelector('#profession');
+let professionInput = document.querySelector('#profession');
 btnAdd.addEventListener('click', () => {
     let name = nameInput.value;
     let age = ageInput.value;
-    let country = countryInput.value;
+    let profession = professionInput.value;
     let template = `
                 <tr>
                     <td>${name}</td>
                     <td>${age}</td>
-                    <td>${country}</td>
+                    <td>${profession}</td>
                 </tr>`;
     table.innerHTML += template;
 });
