c5763baae5090732a0c83ae3b4177322fdc2e1d5
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-01-15 00:46:13 +0530

    from

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-const formEl = document.querySelector("form");
-      const tbodyEl = document.querySelector("tbody");
-      const tableEl = document.querySelector("table");
-      function onAddWebsite(e) {
-        e.preventDefault();
-        const name = document.getElementById("name").value;
-        const profession = document.getElementById("profession").value;
-        const age = document.getElementById("age").value;
-        tbodyEl.innerHTML += `
-            <tr>
-                <td>${url}</td>
-                <td>${website}</td>
-                <td><button class="deleteBtn">Delete</button></td>
-            </tr>
-        `;
-      }
-
-      function onDeleteRow(e) {
-        if (!e.target.classList.contains("deleteBtn")) {
-          return;
-        }
-
-        const btn = e.target;
-        btn.closest("tr").remove();
-      }
-
-      formEl.addEventListener("submit", onAddWebsite);
-      tableEl.addEventListener("click", onDeleteRow);
+let btnAdd = document.querySelector('button');
+let table = document.querySelector('table');
+let nameInput = document.querySelector('#name');
+let ageInput = document.querySelector('#age');
+let countryInput = document.querySelector('#profession');
+btnAdd.addEventListener('click', () => {
+    let name = nameInput.value;
+    let age = ageInput.value;
+    let country = countryInput.value;
+    let template = `
+                <tr>
+                    <td>${name}</td>
+                    <td>${age}</td>
+                    <td>${country}</td>
+                </tr>`;
+    table.innerHTML += template;
+});
